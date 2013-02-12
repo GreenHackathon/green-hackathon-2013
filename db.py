100dbf8ce3a1c01a83cddfcea6f5a299c8fd8310
David Halter <davidhalter88@gmail.com>
2013-02-12 23:32:08 +0100

    final

--- db.py
+++ db.py
@@ -1,41 +1,65 @@
 
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, Float, String, UniqueConstraint, \
+                        ForeignKey
+
+
+URI = 'postgresql://david:david@localhost:5432/green_hackathon'
+engine = sqlalchemy.create_engine(URI)#, echo=True)
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
-from sqlalchemy import Column, Integer, Float, String, UniqueConstraint
 
-
-URI = 'postgresql://scott:tiger@localhost:5432/green_hackathon'
-engine = sqlalchemy.create_engine(URI, echo=True)
 
 class Producer(Base):
     """Like 'EWZ Zuerich'"""
     __tablename__ = 'producer'
+    __table_args__ = (UniqueConstraint('name', 'year'),)
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     year = Column(Integer)
-
-    UniqueConstraint(name, year)
 
     def __init__(self, name, year):
         self.name = name
         self.year = year
 
 
-class Product(Base):
+class Source(Base):
     """For things like 'Gaskraft'"""
-    __tablename__ = 'product'
+    __tablename__ = 'source'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Percent(Base):
-    producer_id = Column(Integer, primary_key=True)
-    product_id = Column(Integer, primary_key=True)
-    percent = Column(Integer)
+    """the percents of the products are stored here"""
+    __tablename__ = 'percent'
+    producer_id = Column(Integer, ForeignKey(Producer.id), primary_key=True)
+    source_id = Column(Integer, ForeignKey(Source.id), primary_key=True)
+    percent = Column(Float)
 
-    def __init__(self, name, year):
-        self.product_id = name
-        self.year = year
+    def __init__(self, producer, source, percent):
+        self.producer_id = producer.id
+        self.source_id = source.id
+        self.percent = percent
+
+
+class City(Base):
+    __tablename__ = 'city'
+    name = Column(String, primary_key=True)
+    zip = Column(Integer, primary_key=True)
+    producer_id = Column(Integer, ForeignKey(Producer.id))
+
+    def __init__(self, city, zip, producer):
+        self.name = city
+        self.zip = zip
+        self.producer_id = producer.id
+
+
+Base.metadata.create_all(engine)
--- stromkennzeichnung.py
+++ stromkennzeichnung.py
@@ -1,4 +1,4 @@
-#?/usr/bin/env python
+#!/usr/bin/env python
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,15 +6,55 @@ import re
 
 import db
 
+
 YEARS = [2011]
 
 URL = "http://www.stromkennzeichnung.ch/de/suche/powera/search/powerc/Supplier.html"
 
-def to_db(zip, city, year, producer, percent):
+
+class NotFoundError(Exception):
+    pass
+
+
+def to_db(zip, city, year, producer, percents):
+    """
+    This code is really ugly, I've never used sqlalchemy before and I really
+    don't get it.
+    """
+    session = db.Session()
     try:
-        pass
-    except:
-        pass
+        _producer = session.query(db.Producer).filter(db.Producer.name == producer).all()[0]
+    except IndexError:
+        _producer = db.Producer(producer, year)
+        session.add(_producer)
+        session.commit()
+
+    try:
+        _city = session.query(db.City).filter(db.City.name == city,
+                                              db.City.zip == zip,
+                                              ).all()[0]
+    except IndexError:
+        _city = db.City(city, zip, _producer)
+        session.add(_city)
+        session.commit()
+
+    for source, percent in percents.items():
+        try:
+            _source = session.query(db.Source).filter(db.Source.name == source).all()[0]
+        except IndexError:
+            _source = db.Source(source)
+            session.add(_source)
+            session.commit()
+        try:
+            _source = session.query(db.Percent).filter(
+                                        db.Percent.producer_id == _producer.id,
+                                        db.Percent.source_id == _source.id
+                                        ).all()[0]
+        except IndexError:
+            _percent = db.Percent(_producer, _source, percent)
+            session.add(_percent)
+            session.commit()
+
 
 
 def get_html(zip, city, year):
@@ -25,30 +65,58 @@ def get_html(zip, city, year):
     r = requests.post(URL, dct)
     return r.text
 
+
 def get_data(zip, city, year):
     def match_bar(html_code):
         match = re.search(r"OverTooltip\('([^']+)', '([\d.]+)%", unicode(html_code))
-        return match.groups()
+        label, percent = match.groups()
+        label = re.sub('_\d+', '', label)
+        return label, percent
 
     html = get_html(zip, city, year)
     b = BeautifulSoup(html)
     content = b.find(class_='tx-cabag-powerdeclaration')
     content = content.find(class_='results')
 
-    producer = content.find_all(class_='hoverEffect')[1].find(class_='label').text
-    bars = content.find(class_='barBox').find_all(class_='bar')
-    return producer, dict([match_bar(b) for b in bars])
+    if content is None:
+        raise NotFoundError('no content for %s, %s, %s' % (zip, city, year))
+
+    row = content.find_all(class_='row hoverEffect')[1]
+    producer = row.find(class_='label').text
+    try:
+        bars = row.find(class_='barBox').find_all(class_='bar')
+    except AttributeError:
+        return producer, {}
+    else:
+        return producer, dict([match_bar(b) for b in bars])
+
 
 def get_cities(file):
-    cities = []
+    cities = {}
     with open(file) as f:
         for line in f:
-            print line
-    return cities
+            fields = unicode(line, 'latin-1').split('\t')
+            try:
+                zip = fields[2]
+                city = fields[4]
+            except IndexError:
+                pass
+            else:
+                if zip not in cities:
+                    cities[zip] = city
+    return sorted(cities.items(), key=lambda x: x[0])
 
 
+#cities = get_cities('plz_l_20130121.txt')
+#print cities[:10], len(cities)
 #data = get_data(8500, 'Frauenfeld', '2011')
+
 for year in YEARS:
-    for zip, city in get_cities('cities.csv'):
-        producer, percent = get_data(zip, city, year)
-        to_db(zip, city, year, producer, percent)
+    for zip, city in get_cities('plz_l_20130121.txt'):
+        try:
+            producer, percents = get_data(zip, city, year)
+        except NotFoundError as e:
+            print '>>>> ' + e.message
+        else:
+            print 'found data for %s, %s' % (zip, city)
+            to_db(zip, city, year, producer, percents)
